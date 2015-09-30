4f584dc0b39ce6e560c14fd0ef5c70c93755d5eb
mygithubforme <dhaivat.dharsandiya@mavs.uta.edu>
2015-09-30 14:47:38 -0500

    1

---
+++ app/assets/javascripts/jscri.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	$("#button_tag1").click(function(event){
+		
+		$.getJSON("demo_json.json", function(result){
+		  		
+		  		var content = "<table>";
+		  		var i;
+		  		content += "<tr><td><strong>" + " Name " + "</strong></td><td><strong>" + " Job " + "</strong></td></tr>";
+          		for (i = 0; i < result.length; i++)
+          		{
+              		content += "<tr><td>" + result[i].name1 + "</td><td>" + result[i].job + "</td></tr>";
+      			}      
+           		content += "</table>";
+
+      			$("#p1").append(content);
+      	});
+	});
+});
