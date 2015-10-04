3249b5008a6951a7bb3c03fc5fc4cda7b8161e03
mygithubforme <dhaivat.dharsandiya@mavs.uta.edu>
2015-10-04 16:46:16 -0500

    das

--- app/assets/javascripts/jscri.js
+++ app/assets/javascripts/jscri.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 	$("#button_tag1").click(function(event){
 		
@@ -6,13 +7,37 @@ $(document).ready(function() {
 		  		var content = "<table>";
 		  		var i;
 		  		content += "<tr><td><strong>" + " Name " + "</strong></td><td><strong>" + " Job " + "</strong></td></tr>";
-          		for (i = 0; i < result.length; i++)
+          		for (i = 1; i < result.length; i++)
           		{
               		content += "<tr><td>" + result[i].name1 + "</td><td>" + result[i].job + "</td></tr>";
       			}      
            		content += "</table>";
 
       			$("#p1").append(content);
+      			var LatLng="Latitude: "+result[0].name1+" "+"Longitude: "+result[0].job
+      			$("#p2").append(LatLng);
+
+      			var myCenter=new google.maps.LatLng(result[0].name1,result[0].job);
+
+      			initialize(myCenter);
       	});
 	});
 });
+
+
+function initialize(myCenter)
+{
+var mapProp = {
+  center:myCenter,
+  zoom:12,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+
+var map=new google.maps.Map(document.getElementById("map1"),mapProp);
+
+var marker=new google.maps.Marker({
+  position:myCenter,
+  });
+
+marker.setMap(map);
+}
